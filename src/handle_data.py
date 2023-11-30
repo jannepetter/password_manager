ac03096f0fa745724f4082c6b11ad62166fa3df0
jannepetter <peterdev80@gmail.com>
2023-11-30 12:02:43 +0200

    add sqlite db with data encryption/decryption

--- src/handle_data.py
+++ src/handle_data.py
@@ -1,51 +1,314 @@
+from sqlite3 import connect
+import secrets
+from string import ascii_letters, digits, punctuation
+from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives import padding
+import os
+import base64
 
 
-def read_data():
-    """
-    Reads data from db and returns a list of user stored passwords with their ids.
-    Could be a list of tuples [(id,description,password)] or something else.
-    """
-    pass
+DB_NAME = "manager.db"
+ENCODING = "utf-8"
+VECTOR_LENGTH = 16
+KEY_ITERATIONS = 5000000
 
-def write_data(description, password):
-    """
-    Writes a new password to db.
-    """
-    pass
 
-def delete_data(id):
-    """
-    Deletes the stored password from database
-    """
-    pass
+def generate_key(password:str, username:str):
+    kdf = PBKDF2HMAC(
+        algorithm=hashes.SHA256(),
+        iterations=KEY_ITERATIONS,
+        salt=username.encode(),
+        length=32,
+        backend=default_backend()
+    )
+    key = kdf.derive(password.encode())
 
-def edit_data():
-    """
-    Edits the stored password
-    """
-    pass
+    return key
 
-def change_login_password():
+def _encrypt(plaintext:str, key, init_vector):
     """
-    Changes the password that user uses for login.
-    """
-    pass
+    Encrypt with aes256.
 
-def login():
+    Padding is also provided, incase the data is smaller than the block size.
     """
-    Makes db readable
+    padder = padding.PKCS7(256).padder()
+    padded_plaintext = padder.update(plaintext.encode(ENCODING)) + padder.finalize()
+    cipher = Cipher(algorithms.AES256(key), modes.CFB(init_vector), backend=default_backend())
+    encryptor = cipher.encryptor()
+    cipher_bytes = encryptor.update(padded_plaintext) + encryptor.finalize()
+    ciphertext = base64.b64encode(cipher_bytes).decode(ENCODING)
 
-    This might have to be done in the ui. Depending on the solution we choose for storing data
-    """
-    pass
+    return ciphertext
 
-def logout():
+def _decrypt(ciphertext, key, init_vector):
     """
-    Makes db unreadable
+    Removes the encryption and the padding. Returns the plaintext
+    """
+    decoded_cipher = base64.b64decode(ciphertext)
+    cipher = Cipher(algorithms.AES256(key), modes.CFB(init_vector), backend=default_backend())
+    decryptor = cipher.decryptor()
+    decrypted_padded_text = decryptor.update(decoded_cipher) + decryptor.finalize()
 
-    This might have to be done in the ui
+    unpadder = padding.PKCS7(256).unpadder()
+    # Unpad the decrypted text
+    decrypted_bytes = unpadder.update(decrypted_padded_text) + unpadder.finalize()
+    decrypted_text = decrypted_bytes.decode(ENCODING)
+    return decrypted_text
+
+def generate_random_password(length):
     """
-    pass
+    Generates random password.
+    """
+    characters = ascii_letters + digits + punctuation
+    password = ''.join(secrets.choice(characters) for _ in range(length))
+    
+    return password
+
+def _decrypt_raw_data(key, rows):
+    data = []
+    for row in rows:
+        id = row[0]
+        init_vector = row[4]
+        description = _decrypt(row[1],key,init_vector)
+        password = _decrypt(row[2],key,init_vector)
+        username = _decrypt(row[3],key,init_vector)
+
+        data.append(
+            (
+                id,
+                description,
+                password,
+                username
+                )
+            )
+    
+    return data
+
+def read_data(key):
+    """
+    Reads data from db and returns a list of tuples containing user data in decrypted form.
+    [(id, description, password, username),]
+    """
+    ok = check_master_password_ok(key)
+
+    if not ok:
+        # return empty list if the key (username & password) are wrong
+        return []
+    
+    conn = connect(DB_NAME)
+    cursor = conn.cursor()
+
+    cursor.execute('''
+        SELECT id, description, password, username, vector
+        FROM passwords
+    ''')
+
+    rows = cursor.fetchall()
+    data = _decrypt_raw_data(key,rows)
+        
+    conn.close()
+
+    return data
+
+def check_master_password_ok(key):
+    """
+    Protects the db and the user from writing data with wrong password to the db.
+
+    Decrypting with wrong password will raise an error and it is important that 
+    all the data is encrypted with the same master password.
+    """
+    conn = connect(DB_NAME)
+    cursor = conn.cursor()
+
+    cursor.execute('''
+        SELECT description, vector
+        FROM passwords
+    ''')
+    rows = cursor.fetchall()
+    try:
+        for row in rows:
+            init_vector = row[1]
+            _decrypt(row[0],key,init_vector)
+    except Exception:
+        conn.close()
+        return False
+    
+    conn.close()
+    return True
+
+
+def write_data(key,description,password,username="")-> bool:
+    """
+    Writes a new entry to db. Returns True if successful
+    """
+    ok = check_master_password_ok(key)
+
+    if not ok:
+        # return if the key (username & password) are wrong
+        return False
+    
+    init_vector = os.urandom(VECTOR_LENGTH)
+    encrypted_description = _encrypt(description,key,init_vector)
+    encrypted_password = _encrypt(password,key,init_vector)
+    encrypted_username = _encrypt(username,key,init_vector)
+
+    conn = connect(DB_NAME)
+    cursor = conn.cursor()
+
+    # Insert data into the table
+    cursor.execute('''
+        INSERT INTO passwords (description, password, username, vector)
+        VALUES (?, ?, ?, ?)
+    ''', (encrypted_description, 
+        encrypted_password, 
+        encrypted_username,
+        init_vector
+        ))
+    conn.commit()
+    conn.close()
+
+    return True
+
+
+def delete_data(key,id)-> bool:
+    """
+    Deletes the entry by id.
+    """
+    ok = check_master_password_ok(key)
+    if not ok:
+        # return if the key (username & password) are wrong
+        return False
+    
+    conn = connect(DB_NAME)
+    cursor = conn.cursor()
+
+    cursor.execute(f"DELETE FROM passwords WHERE id = ?", (id,))
+    conn.commit()
+
+    conn.close()
+
+def edit_data(key,id,description,password,username):
+    """
+    Edits the stored entry
+    """
+    ok = check_master_password_ok(key)
+    if not ok:
+        # return if the key (username & password) are wrong
+        return False
+
+    init_vector = os.urandom(VECTOR_LENGTH)
+    encrypted_description = _encrypt(description,key,init_vector)
+    encrypted_password = _encrypt(password,key,init_vector)
+    encrypted_username = _encrypt(username,key,init_vector)
+
+
+    conn = connect(DB_NAME)
+    cursor = conn.cursor()
+
+    # Insert data into the table
+    cursor.execute('''
+        UPDATE passwords SET description = ?, password = ?, username = ?, vector = ?
+        WHERE id = ?
+    ''', (encrypted_description, 
+          encrypted_password, 
+          encrypted_username,
+          init_vector,
+          id
+          ))
+    conn.commit()
+    conn.close()
+
+def change_login_password(username, password, new_username, new_password):
+    """
+    Changes the password that user uses for encrypting and decrypting. The master password.
+
+    Errors happening in the middle of the process will lead to rollback. The success of the
+    encryption is also tested with each entry that it matches the original data. Incase it 
+    does not, the change of the master password is aborted and rollback occurs.
+    """
+    key = generate_key(password,username)
+
+    ok = check_master_password_ok(key)
+    if not ok:
+        return False
+    
+    original_data = read_data(key)
+    try:
+        new_key = generate_key(new_password,new_username)
+
+        conn = connect(DB_NAME)
+        conn.execute('BEGIN TRANSACTION')
+        cursor = conn.cursor()
+
+        for el in original_data:
+
+            init_vector = os.urandom(VECTOR_LENGTH)
+            id = el[0]
+            description = el[1]
+            password = el[2]
+            username = el[3]
+
+            encrypted_description = _encrypt(description,new_key,init_vector)
+            encrypted_password = _encrypt(password,new_key,init_vector)
+            encrypted_username = _encrypt(username,new_key,init_vector)
+
+            cursor.execute('''
+                UPDATE passwords SET description = ?, password = ?, username = ?, vector = ?
+                WHERE id = ?
+            ''', (encrypted_description, 
+                encrypted_password, 
+                encrypted_username,
+                init_vector,
+                id
+                ))
+        
+        # Data should be identical to original data when decrypted. Raise error if not.
+        
+        cursor.execute('''
+            SELECT id, description, password, username, vector
+            FROM passwords
+        ''')
+
+        rows = cursor.fetchall()
+        data = _decrypt_raw_data(new_key,rows)
+
+        # check that each entry matches the old when decrypted with the new key
+        for i in range(len(original_data)):
+            assert original_data[i][0] == data[i][0], "id is not the same"
+            assert original_data[i][1] == data[i][1], "description is not the same"
+            assert original_data[i][2] == data[i][2], "password is not the same"
+            assert original_data[i][3] == data[i][3], "username is not the same"
+
+        conn.commit()
+        
+    except Exception as e:
+        print('Password change has failed. Rolling back. ',e)
+        conn.rollback()
+        conn.close()
+        return False
+        
+    conn.close()
+    return True
+
+    
+
+def login(password,username):
+    """
+    Returns a key for db actions. 
+    
+    Returns None if the provided username and password are wrong
+    """
+    key = generate_key(password, username)
+    ok = check_master_password_ok(key)
+
+    if ok:
+        return key
+    
+    return None
+
 
 def copy_db_to_location(location_to_save):
     """
@@ -53,16 +316,22 @@ def copy_db_to_location(location_to_save):
     """
     pass
 
-def check_db_exists()->bool:
-    """
-    Function to check if db exists. The ui will at startup check if the 
-    db exists. If not, then it will query users username and login password.
-    After this it will execute db_init()
-    """
-    pass
 
-def db_init(username, login_password):
+def db_init():
     """
     Inits the db if it does not yet exist. E.g user starts the app for the first time.
     """
-    pass
+    conn = connect(DB_NAME)
+    cursor = conn.cursor()
+    CREATE_TABLE_QUERY = '''
+        CREATE TABLE IF NOT EXISTS passwords(
+            id INTEGER PRIMARY KEY,
+            description TEXT,
+            password TEXT,
+            username TEXT,
+            vector BLOB
+        )
+    '''
+    cursor.execute(CREATE_TABLE_QUERY)
+    conn.commit()
+    conn.close()
--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 from ui import UI
+from handle_data import db_init, write_data, read_data, login
 
 
 def main():
@@ -14,8 +15,24 @@ def main():
 
     I have named the requirements file as requirements.txt
     """
-    print("password manager app")
-    ui = UI()
+    password = "example_pass"
+    username = "example_user"
+
+    # example use of the db. Remove when finished.
+    db_init()
+    key = login(password,username)
+    data = {
+        "description":"test description",
+        "password":"some_password",
+        "username":"some_username"
+    }
+    ok = write_data(key, **data)
+    print("write data ok: ", ok)
+
+    data = read_data(key)
+    print('Reading data:')
+    for el in data:
+        print(el)
 
 if __name__ == "__main__":
     main()
